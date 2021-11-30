b31eb9dd9f79c8e77d75e72586e5e3d136669343
Mateus M. Ferraz <mateusferrazf4@gmail.com>
2021-11-30 10:28:14 -0300

    New Vet

--- VeterinaryClinic/src/main/java/View/Vet/NewVetJDialog.java
+++ VeterinaryClinic/src/main/java/View/Vet/NewVetJDialog.java
@@ -6,6 +6,8 @@
 
 package View.Vet;
 
+import Controller.ControllerVet;
+
 /**
  *
  * @author mateu
@@ -21,6 +23,18 @@ public class NewVetJDialog extends javax.swing.JDialog {
         initComponents();
     }
 
+    private String getCurrentVetName() {
+        return this.jTextFieldNewVetName.getText();
+    }
+    
+    private String getCurrentVetPhone() {
+        return this.jFormattedTextFieldNewVetPhone.getText().replace("(","").replace(")","").replace("-","");
+    }
+    
+    private String getCurrentVetAddress() {
+        return this.jTextFieldNewVetAddress.getText();
+    }
+    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -31,108 +45,101 @@ public class NewVetJDialog extends javax.swing.JDialog {
     private void initComponents() {
 
         jPanelFrame = new javax.swing.JPanel();
-        jPanelNewAnimalTittle = new javax.swing.JPanel();
-        jLabelNewAnimalTittle = new javax.swing.JLabel();
-        jPanelNewAnimalForm = new javax.swing.JPanel();
-        jLabelNewAnimalName = new javax.swing.JLabel();
-        jTextFieldNewAnimalName = new javax.swing.JTextField();
-        jLabelNewAnimalBirthYear = new javax.swing.JLabel();
-        jFormattedTextFieldNewAnimalBirthYear = new javax.swing.JFormattedTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jButtonNewAnimalConfirm = new javax.swing.JButton();
+        jPanelNewVetTittle = new javax.swing.JPanel();
+        jLabelNewVetTittle = new javax.swing.JLabel();
+        jPanelNewVetForm = new javax.swing.JPanel();
+        jLabelNewVetName = new javax.swing.JLabel();
+        jTextFieldNewVetName = new javax.swing.JTextField();
+        jLabelNewVetPhone = new javax.swing.JLabel();
+        jFormattedTextFieldNewVetPhone = new javax.swing.JFormattedTextField();
+        jLabelNewVetAddress = new javax.swing.JLabel();
+        jTextFieldNewVetAddress = new javax.swing.JTextField();
+        jButtonNewVetConfirm = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("New Vet");
         setResizable(false);
 
-        jPanelNewAnimalTittle.setLayout(new java.awt.BorderLayout());
+        jPanelNewVetTittle.setLayout(new java.awt.BorderLayout());
 
-        jLabelNewAnimalTittle.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        jLabelNewAnimalTittle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabelNewAnimalTittle.setText("New Vet");
-        jPanelNewAnimalTittle.add(jLabelNewAnimalTittle, java.awt.BorderLayout.CENTER);
+        jLabelNewVetTittle.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabelNewVetTittle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelNewVetTittle.setText("New Vet");
+        jPanelNewVetTittle.add(jLabelNewVetTittle, java.awt.BorderLayout.CENTER);
 
-        jLabelNewAnimalName.setText("Name");
+        jLabelNewVetName.setText("Name");
 
-        jLabelNewAnimalBirthYear.setText("Phone");
+        jLabelNewVetPhone.setText("Phone");
 
         try {
-            jFormattedTextFieldNewAnimalBirthYear.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("(##)#####-####")));
+            jFormattedTextFieldNewVetPhone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("(##)#####-####")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
 
-        jLabel3.setText("Address");
+        jLabelNewVetAddress.setText("Address");
 
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        jButtonNewVetConfirm.setText("Register");
+        jButtonNewVetConfirm.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButtonNewVetConfirm.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                jButtonNewVetConfirmActionPerformed(evt);
             }
         });
 
-        jButtonNewAnimalConfirm.setText("Register");
-        jButtonNewAnimalConfirm.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButtonNewAnimalConfirm.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonNewAnimalConfirmActionPerformed(evt);
-            }
-        });
-
-        org.jdesktop.layout.GroupLayout jPanelNewAnimalFormLayout = new org.jdesktop.layout.GroupLayout(jPanelNewAnimalForm);
-        jPanelNewAnimalForm.setLayout(jPanelNewAnimalFormLayout);
-        jPanelNewAnimalFormLayout.setHorizontalGroup(
-            jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanelNewAnimalFormLayout.createSequentialGroup()
-                .add(jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING, false)
-                        .add(jLabelNewAnimalName, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .add(jLabelNewAnimalBirthYear, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE))
-                    .add(jLabel3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 42, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+        org.jdesktop.layout.GroupLayout jPanelNewVetFormLayout = new org.jdesktop.layout.GroupLayout(jPanelNewVetForm);
+        jPanelNewVetForm.setLayout(jPanelNewVetFormLayout);
+        jPanelNewVetFormLayout.setHorizontalGroup(
+            jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanelNewVetFormLayout.createSequentialGroup()
+                .add(jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(jLabelNewVetAddress, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 52, Short.MAX_VALUE)
+                    .add(jLabelNewVetPhone, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(jLabelNewVetName, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(jTextFieldNewAnimalName)
-                    .add(jFormattedTextFieldNewAnimalBirthYear, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 334, Short.MAX_VALUE)
-                    .add(jTextField2)))
-            .add(jPanelNewAnimalFormLayout.createSequentialGroup()
+                .add(jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jTextFieldNewVetAddress)
+                    .add(jFormattedTextFieldNewVetPhone)
+                    .add(jTextFieldNewVetName)))
+            .add(jPanelNewVetFormLayout.createSequentialGroup()
                 .add(146, 146, 146)
-                .add(jButtonNewAnimalConfirm)
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .add(jButtonNewVetConfirm)
+                .addContainerGap(161, Short.MAX_VALUE))
         );
-        jPanelNewAnimalFormLayout.setVerticalGroup(
-            jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanelNewAnimalFormLayout.createSequentialGroup()
-                .add(jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabelNewAnimalName, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 20, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jTextFieldNewAnimalName, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+        jPanelNewVetFormLayout.setVerticalGroup(
+            jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanelNewVetFormLayout.createSequentialGroup()
+                .add(jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabelNewVetName, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 20, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jTextFieldNewVetName, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabelNewAnimalBirthYear, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 23, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jFormattedTextFieldNewAnimalBirthYear, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .add(jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabelNewVetPhone, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 23, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jFormattedTextFieldNewVetPhone, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jPanelNewAnimalFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 20, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jTextField2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .add(jPanelNewVetFormLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabelNewVetAddress, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 20, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jTextFieldNewVetAddress, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .add(jButtonNewAnimalConfirm))
+                .add(jButtonNewVetConfirm))
         );
 
         org.jdesktop.layout.GroupLayout jPanelFrameLayout = new org.jdesktop.layout.GroupLayout(jPanelFrame);
         jPanelFrame.setLayout(jPanelFrameLayout);
         jPanelFrameLayout.setHorizontalGroup(
             jPanelFrameLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanelNewAnimalTittle, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .add(jPanelNewVetTittle, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanelFrameLayout.createSequentialGroup()
                 .addContainerGap()
-                .add(jPanelNewAnimalForm, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .add(jPanelNewVetForm, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanelFrameLayout.setVerticalGroup(
             jPanelFrameLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(jPanelFrameLayout.createSequentialGroup()
-                .add(jPanelNewAnimalTittle, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 50, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(jPanelNewVetTittle, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 50, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jPanelNewAnimalForm, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(jPanelNewVetForm, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -151,26 +158,30 @@ public class NewVetJDialog extends javax.swing.JDialog {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
-
-    private void jButtonNewAnimalConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNewAnimalConfirmActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButtonNewAnimalConfirmActionPerformed
+    private void jButtonNewVetConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonNewVetConfirmActionPerformed
+        try {
+            ControllerVet.newVet(this.getCurrentVetName(),this.getCurrentVetPhone(),this.getCurrentVetAddress());
+            ControllerVet.showDataTableAll(this.frame.getTableComponentsCollection(),this.frame.getTableComponentsVets());
+            this.dispose();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println(ex);
+            javax.swing.JOptionPane.showMessageDialog(this.frame,ex);
+        }
+    }//GEN-LAST:event_jButtonNewVetConfirmActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonNewAnimalConfirm;
-    private javax.swing.JFormattedTextField jFormattedTextFieldNewAnimalBirthYear;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabelNewAnimalBirthYear;
-    private javax.swing.JLabel jLabelNewAnimalName;
-    private javax.swing.JLabel jLabelNewAnimalTittle;
+    private javax.swing.JButton jButtonNewVetConfirm;
+    private javax.swing.JFormattedTextField jFormattedTextFieldNewVetPhone;
+    private javax.swing.JLabel jLabelNewVetAddress;
+    private javax.swing.JLabel jLabelNewVetName;
+    private javax.swing.JLabel jLabelNewVetPhone;
+    private javax.swing.JLabel jLabelNewVetTittle;
     private javax.swing.JPanel jPanelFrame;
-    private javax.swing.JPanel jPanelNewAnimalForm;
-    private javax.swing.JPanel jPanelNewAnimalTittle;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextFieldNewAnimalName;
+    private javax.swing.JPanel jPanelNewVetForm;
+    private javax.swing.JPanel jPanelNewVetTittle;
+    private javax.swing.JTextField jTextFieldNewVetAddress;
+    private javax.swing.JTextField jTextFieldNewVetName;
     // End of variables declaration//GEN-END:variables
 
 }
